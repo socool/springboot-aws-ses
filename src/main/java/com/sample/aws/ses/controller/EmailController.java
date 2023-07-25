e41a3568cd0a8a702a09bc32ceee5200595749f9
So Cool <127.0.0.1.pm@gmail.com>
2023-07-25 10:09:24 +0700

    add sample and send logic

---
+++ src/main/java/com/sample/aws/ses/controller/EmailController.java
@@ -0,0 +1,28 @@
+package com.sample.aws.ses.controller;
+
+import com.sample.aws.ses.model.EmailDetails;
+import com.sample.aws.ses.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/sendEmail")
+    public String sendMessage(@RequestBody EmailDetails emailDetails) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(emailDetails.getFromEmail());
+        simpleMailMessage.setTo(emailDetails.getToEmail());
+        simpleMailMessage.setSubject(emailDetails.getSubject());
+        simpleMailMessage.setText(emailDetails.getBody());
+        emailService.sendMessage(simpleMailMessage);
+
+        return "Email sent successfully";
+    }
+}
---
+++ src/main/java/com/sample/aws/ses/model/EmailDetails.java
@@ -0,0 +1,42 @@
+package com.sample.aws.ses.model;
+
+public class EmailDetails {
+    private String fromEmail;
+    private String toEmail;
+
+    private String subject;
+
+    private String body;
+
+    public String getFromEmail() {
+        return fromEmail;
+    }
+
+    public void setFromEmail(String fromEmail) {
+        this.fromEmail = fromEmail;
+    }
+
+    public String getToEmail() {
+        return toEmail;
+    }
+
+    public void setToEmail(String toEmail) {
+        this.toEmail = toEmail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
